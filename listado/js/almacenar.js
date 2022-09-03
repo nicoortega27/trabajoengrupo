f599c044f5b1d8ec1d3df48b23f816dd7d3edcb6
nicoortega27 <nikkoortega2015@gmail.com>
2022-09-03 14:00:37 -0300

    grupal pronto

---
+++ listado/js/almacenar.js
@@ -0,0 +1,60 @@
+// funcion guardar//
+function guardar() {
+    localStorage.setItem('contenido', JSON.stringify(archivo))
+}
+// funcion mostrar//
+function mostrar() {
+    let archivo2 = localStorage.getItem('contenido')
+    console.log(archivo2)
+    archivo = JSON.parse(archivo2)
+}
+
+// funcion para agregar datos//
+let archivo = []
+
+function agregar() {
+    let item = document.getElementById("item").value
+    if (item !== "") {
+        archivo.push(item)
+        guardar(archivo);
+        document.getElementById("item").value = "";
+    }
+    añadir();
+}
+// funcion añadir//
+function añadir() {
+    let filas = ""
+
+    for (let item of archivo) {
+        filas += `<li class="list-group-item"> ${item} </li>`
+    }
+    document.getElementById("contenedor").innerHTML = filas
+}
+// final de la funcion añadir //
+
+document.addEventListener("DOMContentLoaded", () => {
+    mostrar();
+    añadir();
+    document.getElementById("agregar").addEventListener("click", () => {
+        agregar();
+        console.log(archivo)
+    })
+
+}
+)
+// funcion que limpia los datos de contenedor//
+function limpia() {
+
+    archivo = []
+    guardar();
+    añadir();
+
+
+
+}
+document.addEventListener("DOMContentLoaded", () => {
+
+    let botoncito = document.getElementById("limpiar").addEventListener("click", () => {
+        limpia();
+    })
+})
